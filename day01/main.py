1cc1d012f92a56b49021bed912e240ec43fd851b
Joey Gibson <joey@joeygibson.com>
2021-12-01 12:42:22 -0500

    day 1, part 2

--- day01/main.py
+++ day01/main.py
@@ -1,17 +1,31 @@
-# /usr/bin/env python3
-
-# Press the green button in the gutter to run the script.
+#! /usr/bin/env python3
 import sys
 
 
-def part1(path: str) -> None:
+def part1(path: str) -> int:
     with open(path) as f:
         lines = [int(x) for x in f.readlines()]
 
     res = [ind + 1 for ind, x in enumerate(lines) if ind + 1 < len(lines) and lines[ind + 1] > x]
-    print(res)
-    print(len(res))
+    return len(res)
+
+
+def part2(path: str) -> int:
+    with open(path) as f:
+        lines = [int(x) for x in f.readlines()]
+
+    groups = [x + lines[ind + 1] + lines[ind + 2] for ind, x in enumerate(lines) if ind + 2 < len(lines)]
+    res = [ind + 1 for ind, x in enumerate(groups) if ind + 1 < len(groups) and groups[ind + 1] > x]
+    return len(res)
 
 
 if __name__ == '__main__':
-    part1(sys.argv[1])
+    if len(sys.argv) == 1:
+        print('Usage: main.py <filename>')
+        sys.exit(1)
+
+    r1 = part1(sys.argv[1])
+    r2 = part2(sys.argv[1])
+
+    print(f'part 1: {r1}')
+    print(f'part 2: {r2}')
