ef359b5a40d6ffe7c3cbd1584b874ff8f8a834d7
Joey Gibson <joey@joeygibson.com>
2021-12-02 12:32:45 -0500

    day 2, part 1

---
+++ day02/main.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+import sys
+
+
+def part1(lst: list) -> int:
+    horiz_ops = filter(lambda x: x[0] == 'forward', lst)
+    vert_ops = filter(lambda x: x[0] != 'forward', lst)
+
+    horizontal = sum([int(x[1]) for x in horiz_ops])
+    vertical = sum([int(x[1]) if x[0] == 'down' else -int(x[1]) for x in vert_ops])
+
+    return horizontal * vertical
+
+
+def get_data(path):
+    with open(path) as f:
+        return [x.strip().split() for x in f.readlines() if x.strip()]
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print('Usage: main.py <filename>')
+        sys.exit(1)
+
+    lines = get_data(sys.argv[1])
+
+    r1 = part1(lines)
+    # r2 = part2(lines)
+
+    print(f'part 1: {r1}')
+    # print(f'part 2: {r2}')
