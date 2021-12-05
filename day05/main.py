b7d0f36144d9e9c6a17c0e846eb48f1587b29344
Joey Gibson <joey@joeygibson.com>
2021-12-05 17:14:32 -0500

    day 5, refactoring

--- day05/main.py
+++ day05/main.py
@@ -3,37 +3,7 @@ import itertools
 import sys
 
 
-def part1(lst: list) -> int:
-    straight_lines = list(filter(lambda x: x[0][0] == x[1][0] or x[0][1] == x[1][1], lst))
-
-    xs = [int(x[0][0]) for x in straight_lines] + [int(x[1][0]) for x in straight_lines]
-    max_x = max(xs)
-    ys = [int(x[0][1]) for x in straight_lines] + [int(x[1][1]) for x in straight_lines]
-    max_y = max(ys)
-
-    sea_map = {}
-    for x in range(0, max_x + 1):
-        for y in range(0, max_y + 1):
-            sea_map[(x, y)] = '.'
-
-    for a, b in straight_lines:
-        if a[0] == b[0]:
-            step = 1 if a[1] < b[1] else -1
-            line = [(a[0], y) for y in range(a[1], b[1] + step, step)]
-        else:
-            step = 1 if a[0] < b[0] else -1
-            line = [(x, a[1]) for x in range(a[0], b[0] + step, step)]
-
-        for pair in line:
-            if sea_map[pair] == '.':
-                sea_map[pair] = 1
-            else:
-                sea_map[pair] += 1
-
-    return len(list(filter(lambda v: v != '.' and v > 1, sea_map.values())))
-
-
-def part2(lst: list) -> int:
+def find_clouds(lst: list) -> int:
     xs = [int(x[0][0]) for x in lst] + [int(x[1][0]) for x in lst]
     max_x = max(xs)
     ys = [int(x[0][1]) for x in lst] + [int(x[1][1]) for x in lst]
@@ -55,8 +25,8 @@ def part2(lst: list) -> int:
         else:
             fill_value = None
 
-        co = list(itertools.zip_longest(range(a[0], b[0] + x_step, x_step), range(a[1], b[1] + y_step, y_step),
-                                        fillvalue=fill_value))
+        co = itertools.zip_longest(range(a[0], b[0] + x_step, x_step), range(a[1], b[1] + y_step, y_step),
+                                   fillvalue=fill_value)
 
         for pair in co:
             if sea_map[pair] == '.':
@@ -80,8 +50,8 @@ if __name__ == '__main__':
 
     lines = get_data(sys.argv[1])
 
-    r1 = part1(lines)
-    r2 = part2(lines)
+    r1 = find_clouds(list(filter(lambda x: x[0][0] == x[1][0] or x[0][1] == x[1][1], lines)))
+    r2 = find_clouds(lines)
 
     print(f'part 1: {r1}')
     print(f'part 2: {r2}')
