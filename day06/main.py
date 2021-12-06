bf03154d61db12712d38368ca05372128362e4f3
Joey Gibson <joey@joeygibson.com>
2021-12-06 17:08:10 -0500

    day 6, part 2

--- day06/main.py
+++ day06/main.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 import sys
+from collections import Counter
 
 
 class Lanternfish:
@@ -34,9 +35,32 @@ def part1(fish: list[Lanternfish], iterations: int) -> int:
     return len(fish)
 
 
-def get_data(path) -> list:
+def part2(fish: list[int], iterations: int) -> int:
+    fish_counts = Counter(fish)
+
+    for i in range(0, iterations):
+        for k, v in sorted(fish_counts.items(), key=lambda pair: pair[0]):
+            print(f'{k}: {v}', end=', ')
+
+        births = fish_counts.get(0) if fish_counts.get(0) is not None else 0
+        print(f' births {births}')
+        for day in range(1, 9):
+            fish_counts[day - 1] = fish_counts.get(day) if fish_counts.get(day) is not None else 0
+
+        fish_counts[8] = births
+        fish_counts[6] = fish_counts.get(6) + births
+
+    return sum(fish_counts.values())
+
+
+def get_as_fish(path) -> list:
     with open(path) as f:
         return [Lanternfish(x) for x in map(int, f.read().strip().split(','))]
+
+
+def get_as_ints(path) -> list:
+    with open(path) as f:
+        return [int(x) for x in f.read().strip().split(',')]
 
 
 if __name__ == '__main__':
@@ -44,8 +68,11 @@ if __name__ == '__main__':
         print('Usage: main.py <filename> <iterations>')
         sys.exit(1)
 
-    fish = get_data(sys.argv[1])
-    r1 = part1(fish, int(sys.argv[2]))
+    file_name = sys.argv[1]
+    iterations = int(sys.argv[2])
 
-    print(f'part 1: {r1}')
-    # print(f'part 2: {r2}')
+    # r1 = part1(get_as_fish(file_name), iterations)
+    r2 = part2(get_as_ints(file_name), iterations)
+
+    # print(f'part 1: {r1}')
+    print(f'part 2: {r2}')
