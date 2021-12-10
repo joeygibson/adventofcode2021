596029cb611049db360c7eecdb3907149ed1bc93
Joey Gibson <joey@joeygibson.com>
2021-12-10 13:13:44 -0500

    day 10, part 2

--- day10/main.py
+++ day10/main.py
@@ -14,15 +14,16 @@ OPENERS = [L_PAREN, L_BRACKET, L_CURLY, L_ANGLE]
 CLOSERS = [R_PAREN, R_BRACKET, R_CURLY, R_ANGLE]
 
 
-def part1(rows: list[list[str]]) -> int:
+def part1(rows: list[list[str]]) -> (int, list[str]):
     offenders = {}
+    offending_rows = []
     for row in rows:
-        print(row)
         stack = []
         for ch in row:
             if ch in CLOSERS:
                 if len(stack) == 0:
                     offenders[ch] = 1
+                    offending_rows.append(row)
                     break
                 else:
                     top = stack[-1]
@@ -31,36 +32,51 @@ def part1(rows: list[list[str]]) -> int:
                             (ch == R_CURLY and top != L_CURLY) or \
                             (ch == R_ANGLE and top != L_ANGLE):
                         offenders[ch] = offenders.get(ch, 0) + 1
+                        offending_rows.append(row)
                         break
                     else:
                         stack.pop()
             else:
                 stack.append(ch)
 
-    print(f'stack {stack}')
-    print(f'offenders {offenders}')
-
     parens = offenders.get(R_PAREN, 0) * 3
     brackets = offenders.get(R_BRACKET, 0) * 57
     curlies = offenders.get(R_CURLY, 0) * 1197
     angles = offenders.get(R_ANGLE, 0) * 25137
 
-    return sum([parens, brackets, curlies, angles])
+    return sum([parens, brackets, curlies, angles]), offending_rows
 
 
-def is_valid(row: list[str]) -> bool:
-    chars = {'parens': 0, 'brackets': 0, 'curlies': 0, 'angles': 0}
-    for ch in chars:
-        if ch in [L_PAREN, R_PAREN]:
-            chars['parens'] += 1
-        elif ch in [L_BRACKET, R_BRACKET]:
-            chars['brackets'] += 1
-        elif ch in [L_CURLY, R_CURLY]:
-            chars['curlies'] += 1
-        else:
-            chars['angles'] += 1
+def part2(rows: list[list[str]]) -> int:
+    scores = []
 
-    return all([x % 2 == 0 for x in chars.values()])
+    for row in rows:
+        stack = []
+        for ch in row:
+            if ch in CLOSERS:
+                stack.pop()
+            else:
+                stack.append(ch)
+
+        score = 0
+
+        for ch in reversed(stack):
+            if ch == L_PAREN:
+                ch_score = 1
+            elif ch == L_BRACKET:
+                ch_score = 2
+            elif ch == L_CURLY:
+                ch_score = 3
+            else:
+                ch_score = 4
+
+            score = (score * 5) + ch_score
+
+        scores.append(score)
+
+    sorted_scores = sorted(scores)
+
+    return sorted_scores[int(len(sorted_scores) / 2)]
 
 
 def get_data(path) -> list[list[str]]:
@@ -76,5 +92,10 @@ if __name__ == '__main__':
     file_name = sys.argv[1]
     data = get_data(file_name)
 
-    print(f'part 1: {part1(get_data(file_name))}')
-    # print(f'part 2: {part2(get_data(file_name))}')
+    p1, offending_rows = part1(get_data(file_name))
+
+    print(f'part 1: {p1}')
+
+    good_data = [row for row in data if row not in offending_rows]
+
+    print(f'part 2: {part2(good_data)}')
