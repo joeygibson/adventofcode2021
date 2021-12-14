66b0a534e30f174fa8409b355d18816ebf8609ce
Joey Gibson <joey@joeygibson.com>
2021-12-14 13:11:26 -0500

    day 14, part 1

---
+++ day14/main.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python3
+import itertools
+import sys
+from collections import Counter
+
+
+def get_data(path) -> (list[str], dict[str, str]):
+    with open(path) as f:
+        lines = f.readlines()
+
+    template = [c for c in lines[0].strip()]
+
+    # rules = {splits[0].strip(): splits[1].strip()
+    #          for line in itertools.dropwhile(lambda x: '->' not in x, lines)
+    #          for splits in line.split(' -> ')}
+    rules = {}
+    for line in itertools.dropwhile(lambda x: '->' not in x, lines):
+        splits = line.strip().split(' -> ')
+        rules[splits[0].strip()] = splits[1].strip()
+
+    return template, rules
+
+
+def partition(lst: list, n: int) -> list:
+    for i in range(0, len(lst) - 1):
+        yield lst[i:i + 2]
+
+
+def part1(template: list[str], rules: dict[str, str]) -> int:
+    current = template
+
+    for i in range(10):
+        print(f'iteration {i + 1}')
+        new = []
+
+        pairs = list(partition(current, 1))
+        new_vals = [rules[''.join(pair)] for pair in pairs]
+        for index, c in enumerate(current):
+            if index >= len(new_vals):
+                new += [c]
+            else:
+                new += [c, new_vals[index]]
+
+        current = new
+
+    counts = Counter(current)
+
+    max_elem = max(counts.values())
+    min_elem = min(counts.values())
+
+    return max_elem - min_elem
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Usage: main.py <filename>')
+        sys.exit(1)
+
+    file_name = sys.argv[1]
+
+    print(f'part1 {part1(*get_data(file_name))}')
