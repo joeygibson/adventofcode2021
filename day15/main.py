192ac54f13a5c3bc4130432605c33f685c0fd018
Joey Gibson <joey@joeygibson.com>
2021-12-15 23:23:12 -0500

    day 15, non-working a*

--- day15/main.py
+++ day15/main.py
@@ -1,45 +1,108 @@
 #! /usr/bin/env python3
 import sys
+from typing import Tuple
 
-from collections import deque
+Pair = Tuple[int, int]
 
 
-def get_data(path) -> (list[list[int]]):
+class Spot:
+    def __init__(self, position: Pair, parent: 'Spot' = None, risk: int = 0):
+        self.position = position
+        self.parent = parent
+        self.g = 0
+        self.h = 0
+        self.f = 0
+        self.risk = risk
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+    def __lt__(self, other):
+        return self.risk < other.risk
+
+    def __repr__(self):
+        return f'({self.position}, {self.f} ({self.risk}))'
+
+
+def get_data(path) -> dict[Pair, int]:
     with open(path) as f:
         lines = f.readlines()
 
-    return [[int(i) for i in r.strip()] for r in lines]
+    the_map = {}
+
+    for j, row in enumerate(lines):
+        for i, val in enumerate(row.strip()):
+            the_map[(i, j)] = int(val)
+
+    return the_map
 
 
-def part1(x: list[list[int]]) -> int:
-    t = 1e99
+def part1(the_map: dict[Pair, int]) -> [Pair]:
+    start_pos = min(the_map.keys())
+    goal_pos = max(the_map.keys())
 
-    q = deque([(0, 0, 0)])
-    v = set()
+    start = Spot(start_pos, risk=the_map[start_pos])
+    goal = Spot(goal_pos, risk=the_map[goal_pos])
 
-    while q:
-        a, b, c = q.popleft()
+    open = []
+    closed = []
 
-        if (a, b, c) in v:
-            continue
+    open.append(start)
 
-        v.add((a, b, c))
+    while len(open) > 0:
+        open.sort()
 
-        if (a, b) == (len(x) - 1, len(x[0]) - 1):
-            t = min(t, c)
-            continue
+        current = open.pop(0)
 
-        try:
-            q.append((a + 1, b, c + x[a + 1][b]))
-        except:
-            pass
+        closed.append(current)
 
-        try:
-            q.append((a, b + 1, c + x[a][b + 1]))
-        except:
-            pass
+        if current == goal:
+            path = []
+            while current != start:
+                path.append(current.position)
+                current = current.parent
 
-    return t
+            for spot in reversed(path):
+                print(f'{spot} => {the_map[spot]}')
+
+            res = sum([the_map[spot] for spot in path])
+
+            return res
+
+        x, y = current.position
+
+        neighbors = [(x + 1, y), (x, y + 1)]
+
+        for next in neighbors:
+            map_value = the_map.get(next)
+
+            if map_value is None:
+                continue
+
+            neighbor = Spot(next, current, risk=map_value)
+
+            if neighbor is closed:
+                continue
+
+            neighbor.g = abs(neighbor.position[0] - start.position[0]) + \
+                         abs(neighbor.position[1] - start.position[1])
+            neighbor.h = abs(neighbor.position[0] - goal.position[0]) + \
+                         abs(neighbor.position[1] - goal.position[1])
+            neighbor.f = neighbor.g + neighbor.h + neighbor.risk
+
+            if add_to_open(open, neighbor):
+                open.append(neighbor)
+
+    return None
+
+
+def add_to_open(open: list[Spot], neighbor: Spot) -> bool:
+    for node in open:
+        print(f'{neighbor.f} <=> {node.f}')
+        if neighbor == node and neighbor.f >= node.f:
+            return False
+
+    return True
 
 
 # def part1(the_map: dict[Tuple[int, int], int]) -> int:
